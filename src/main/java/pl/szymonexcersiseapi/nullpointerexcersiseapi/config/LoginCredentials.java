81547ef364d5f9575f24a516392126e346b2aa7a
szymon <szymonstankowski1@gmail.com>
2022-04-05 08:49:00 +0200

    Dodanie controllera logowania i konfiguracji spring security authentication filter nie dziala(nie wyswietla swaggera) :|

---
+++ src/main/java/pl/szymonexcersiseapi/nullpointerexcersiseapi/config/LoginCredentials.java
@@ -0,0 +1,9 @@
+package pl.szymonexcersiseapi.nullpointerexcersiseapi.config;
+
+import lombok.Getter;
+
+@Getter
+public class LoginCredentials {
+    private String username;
+    private String password;
+}
