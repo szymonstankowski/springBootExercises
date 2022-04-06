8d69756e5308bbc79b724f2ca3dc8d59659eae3e
szymon <szymonstankowski1@gmail.com>
2022-04-06 14:18:08 +0200

    response.addHeader("Authorization", "Bearer "+ token); nie dzialam token nie chce sie zautoryzowac w swagger i wywala 401 po zalogowaniu sie i nastepnie probie pobrania postow

--- src/main/java/pl/szymonexcersiseapi/nullpointerexcersiseapi/config/RestAuthenticationSuccessHandler.java
+++ src/main/java/pl/szymonexcersiseapi/nullpointerexcersiseapi/config/RestAuthenticationSuccessHandler.java
@@ -2,6 +2,8 @@ package pl.szymonexcersiseapi.nullpointerexcersiseapi.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -20,7 +22,9 @@ public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
     private final long expirationTime;
     private final String secret;
 
-    public RestAuthenticationSuccessHandler(long expirationTime, String secret) {
+    public RestAuthenticationSuccessHandler(
+            @Value("${jwt.expirationTime}") long expirationTime,
+            @Value("${jwt.secret}") String secret) {
         this.expirationTime = expirationTime;
         this.secret = secret;
     }
--- src/main/java/pl/szymonexcersiseapi/nullpointerexcersiseapi/config/SecurityConfig.java
+++ src/main/java/pl/szymonexcersiseapi/nullpointerexcersiseapi/config/SecurityConfig.java
@@ -2,19 +2,25 @@ package pl.szymonexcersiseapi.nullpointerexcersiseapi.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import javax.sql.DataSource;
 
 @Configuration
-@RequiredArgsConstructor
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
@@ -22,6 +28,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final ObjectMapper objectMapper;
     private final RestAuthenticationSuccessHandler successHandler;
     private final RestAuthenticationSuccessFailureHandler failureHandler;
+    private final String secret;
+
+    public SecurityConfig(DataSource dataSource,
+                          ObjectMapper objectMapper,
+                          RestAuthenticationSuccessHandler successHandler,
+                          RestAuthenticationSuccessFailureHandler failureHandler,
+                          @Value("{$jwt.secret}") String secret) {
+        this.dataSource = dataSource;
+        this.objectMapper = objectMapper;
+        this.successHandler = successHandler;
+        this.failureHandler = failureHandler;
+        this.secret = secret;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -40,7 +59,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/v2/api-docs", "/webjars/**", "/swagger-resources/**", "/h2-console/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .addFilter(authenticationFilter())
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(), userDetailsManager(),secret))
                 .exceptionHandling()
                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 .and()
@@ -57,5 +79,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         authenticationFilter.setAuthenticationFailureHandler(failureHandler);
         authenticationFilter.setAuthenticationManager(super.authenticationManager());
         return authenticationFilter;
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(){
+        return new JdbcUserDetailsManager(dataSource);
     }
 }
